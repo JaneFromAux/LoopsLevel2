60de7db80e48898fce04a0c3d2d6c3a537f7414f
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-20 16:27:44 +0200

    Aufgaben erledigt

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+
+
+
+console.log('Level2_1');
+
+let imageArray = () => {
+    let returnArray = [];
+    console.log(returnArray);
+    for (let i = 1; i <= 100; i++) {
+        if (i < 10) {
+            returnArray.push("image_00" + i + ".jpg");
+        } else if (i < 100) {
+            returnArray.push("image_0" + i + ".jpg");
+        } else {
+            returnArray.push("image_" + i + ".jpg");
+        }
+    } console.log(returnArray);
+}
+
+imageArray();
+
+//Level2_2
+let input = document.getElementById('inputLoop');
+let output = document.getElementById('output');
+let arr = [];
+let loop = () => {
+    // console.log("loop loops");
+    let inputty = Number(input.value);
+    // console.log(inputty);
+    if (inputty == 0) {
+        output.innerHTML = "Lp";
+    } else {
+        for (let o = 1; o <= inputty; o++) {
+            arr.push("o");
+        }
+        output.innerHTML = ` L${arr.join("")}p`;
+    }
+}
